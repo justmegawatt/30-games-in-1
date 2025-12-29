f17b5488fe48bc28efd32a2f4639e8e92c61e031
justmegawatt <megawatt@live.com>
2025-12-29 14:37:41 -0500

    Adds new Fruit Slicey scene

--- scripts/main_menu/game_button.gd
+++ scripts/main_menu/game_button.gd
@@ -5,6 +5,7 @@ extends TextureRect
 
 #current games made
 var starting_ui = preload("res://scenes/starting_ui.tscn")
+var fruity_slicey = preload("res://scenes/FruitySlicey.tscn")
 
 func _ready() -> void:
 	$BG/LabelTitle.text = game_name
@@ -14,6 +15,8 @@ func _on_button_play_pressed() -> void:
 	var picked_game
 	if game_name == "starting ui":
 		picked_game = starting_ui.instantiate()
+	if game_name == "Fruity Slicey":
+		picked_game = fruity_slicey.instantiate()
 	$"../../../../../..".add_child(picked_game) #Main
 	picked_game.global_position = Vector2(0, 0)
 	$"../../../..".visible = false #CanvasLayer
